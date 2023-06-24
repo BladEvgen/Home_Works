c46b01bc27912b776a66b2cf44934a16927806e4
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2023-06-24 13:34:58 +0600

    Update main.py

--- new/main.py
+++ new/main.py
@@ -8,7 +8,6 @@ import aiohttp
 import time
 
 
-# TODO: все же есть проблема небольшая, интерфес подвисает во время скачивания, думал проблема во wrapper и вывел в Gui но ничего не поменялось
 url = "https://picsum.photos/320/240/"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) "
@@ -18,15 +17,19 @@ headers = {
 local_path = os.path.join(os.path.dirname(__file__), "temp")
 os.makedirs(local_path, exist_ok=True)
 
+root = tk.Tk()
+root.geometry("500x250")
+
+time_label = tk.Label(root, text="Execution time: ", font=("Arial Bold", 14))
+time_label.pack()
+
 
 def execution_time(function):
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = function(*args, **kwargs)
         execution_time = time.perf_counter() - start_time
-        time_label[
-            "text"
-        ] = f"Execution time: {execution_time:.7f} seconds"  # узнал у нейросети как вывести wrapper в GUI
+        time_label["text"] = f"Execution time: {execution_time:.7f} seconds"
         return result
 
     return wrapper
@@ -40,8 +43,42 @@ def sync_download_images():
             f.write(response.content)
 
 
+async def download_image(session, index):
+    async with session.get(url) as response:
+        with open(os.path.join(local_path, f"image_async_{index}.jpg"), "wb") as f:
+            while True:
+                chunk = await response.content.read(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+
+
 @execution_time
-def threaded_download_images():
+async def async_download_images():
+    async with aiohttp.ClientSession(headers=headers) as session:
+        tasks = []
+        for i in range(10):
+            task = asyncio.create_task(download_image(session, i))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+
+def download_image_multiprocess(index):
+    response = requests.get(url=url, headers=headers)
+    filename = f"image_multiprocess_{index}.jpg"
+    file_path = os.path.join(local_path, filename)
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+
+@execution_time
+def download_images_multiprocess():
+    with Pool(processes=10) as pool:
+        pool.map(download_image_multiprocess, range(10))
+
+@execution_time
+def sync_download_images_threaded():
     def download_image(index):
         response = requests.get(url, headers=headers)
         with open(os.path.join(local_path, f"image_thread_{index}.jpg"), "wb") as f:
@@ -57,51 +94,7 @@ def threaded_download_images():
         thread.join()
 
 
-@execution_time
-async def async_download_images():
-    async with aiohttp.ClientSession(headers=headers) as session:
-        tasks = []
-        for i in range(10):
-            task = asyncio.create_task(download_image(session, i))
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
-
-
-async def download_image(session, index):
-    async with session.get(url) as response:
-        with open(os.path.join(local_path, f"image_async_{index}.jpg"), "wb") as f:
-            while True:
-                chunk = await response.content.read(1024)
-                if not chunk:
-                    break
-                f.write(chunk)
-
-
-# данную функцию я написал отдельным файлом и проверил, также была все таки не большая проблема в области видемости if name == main исправил все (планировал добавить его чуть позже когда все будет работать)
-def download_image_multiprocess(index):
-    response = requests.get(url=url, headers=headers)
-    filename = f"image_multiprocess_{index}.jpg"
-    file_path = os.path.join(local_path, filename)
-    with open(file_path, "wb") as file:
-        file.write(response.content)
-
-
-# этим способом я пользовался и примерно понимал как он работате поэтому решил использовать его, тк с изначальным вариантом с process = multiprocessing.Process(target=sync_download_one_image, args=(), kwargs={}) он создавал 10 копий окон
-# потом по нажатию скачивания мог скачать все 10 файлов
-@execution_time
-def download_images_multiprocess():
-    with Pool(processes=10) as pool:
-        pool.map(download_image_multiprocess, range(10))
-
-
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("500x250")
-
-    time_label = tk.Label(root, text="Execution time: ", font=("Arial Bold", 14))
-    time_label.pack()
-
     sync_button = tk.Button(
         root,
         text="Sync Download",
@@ -113,7 +106,7 @@ if __name__ == "__main__":
     threaded_button = tk.Button(
         root,
         text="Threaded Download",
-        command=threaded_download_images,
+        command=sync_download_images_threaded,
         font=("Arial Bold", 20),
     )
     threaded_button.pack()
