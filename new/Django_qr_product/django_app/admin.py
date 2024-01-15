ed114ad6585411b4491d945e15d706ce7e2ac868
Evgeniy <ekozlov00@mail.ru>
2024-01-15 21:09:36 +0600

    registered all models

--- new/Django_qr_product/django_app/admin.py
+++ new/Django_qr_product/django_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Winery, Wine, WineReview, WineCategory
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Wine)
+admin.site.register(Winery)
+admin.site.register(WineReview)
+admin.site.register(WineCategory)
