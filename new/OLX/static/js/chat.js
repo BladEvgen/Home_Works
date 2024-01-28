28348e9bde6107a55d876884e80a473c975eac0c
Evgeniy <ekozlov00@mail.ru>
2024-01-28 13:45:19 +0600

    design fixed for secusername

--- new/OLX/static/js/chat.js
+++ new/OLX/static/js/chat.js
@@ -1,85 +1,101 @@
-const roomName = JSON.parse(document.getElementById('json-roomname').textContent);
-const userName = JSON.parse(document.getElementById('json-username').textContent);
-const Token = JSON.parse(document.getElementById('json-token').textContent);
-const chatSocket = new WebSocket('ws://' + window.location.host + '/ws/chat/' + roomName + '/?token=' + Token);
-const messageInputDom = document.querySelector('#chat-message-input');
-const messageSubmitButton = document.querySelector('#chat-message-submit');
+const roomName = JSON.parse(
+  document.getElementById("json-roomname").textContent
+);
+const userName = JSON.parse(
+  document.getElementById("json-username").textContent
+);
+const Token = JSON.parse(document.getElementById("json-token").textContent);
+const chatSocket = new WebSocket(
+  "ws://" + window.location.host + "/ws/chat/" + roomName + "/?token=" + Token
+);
+const messageInputDom = document.querySelector("#chat-message-input");
+const messageSubmitButton = document.querySelector("#chat-message-submit");
 
 let dataMessages = {
-    avafiruser: '',
-    avasecuser: '',
-    firusername: '',
-    secusername: '',
-}
+  avafiruser: "",
+  avasecuser: "",
+  firusername: "",
+  secusername: "",
+};
 
-messageInputDom.addEventListener('input', function() {
-    const message = messageInputDom.value.trim();
-    messageSubmitButton.disabled = message === '';
+messageInputDom.addEventListener("input", function () {
+  const message = messageInputDom.value.trim();
+  messageSubmitButton.disabled = message === "";
 });
 
-chatSocket.onmessage = function(e) {
-    const data = JSON.parse(e.data);
-    console.log('Message received:', data);
+chatSocket.onmessage = function (e) {
+  const data = JSON.parse(e.data);
+  console.log("Message received:", data);
 
-    if (data.message && data.message.trim() !== '') {
+  if (data.message && data.message.trim() !== "") {
+    const isCurrentUser = data.username === userName;
+    const isSecUserMessage = data.secusername === userName;
 
-        const htmlRaw = `<div class="flex ${(data.firusername == data.secusername) ? 'flex-row-reverse' : 'flex-row'} gap-x-2">
-            <img src="${(data.firusername == data.secusername) ? data.avafiruser : data.avasecuser }" alt="IMG" class="w-10 h-10 rounded-full">
-            <div class="flex flex-col rounded-xl gap-y-2 p-2 ${(data.firusername == data.secusername) ? 'bg-violet-300' : 'bg-gray-200'}">
+    const htmlRaw = `<div class="flex ${
+      isCurrentUser || isSecUserMessage ? "flex-row-reverse" : "flex-row"
+    } gap-x-2">
+            <div class="flex flex-col rounded-xl gap-y-2 p-2 ${
+              isCurrentUser || isSecUserMessage
+                ? "bg-violet-300"
+                : "bg-gray-200"
+            }">
                 <div class="flex flex-row gap-x-3">
                     <span class="font-bold">${data.username}</span>
                 </div>
                 <span>${data.message}</span>
             </div>
         </div>`;
-        document.querySelector('#chat-messages').innerHTML += htmlRaw;
-        scrollToBottom();
-    }
+
+    document.querySelector("#chat-messages").innerHTML += htmlRaw;
+    scrollToBottom();
+  }
 };
 
-chatSocket.onclose = function(e) {
-    console.log("WebSocket closed:", e);  
-    console.log("Bye!");
+chatSocket.onclose = function (e) {
+  console.log("WebSocket closed:", e);
+  console.log("Bye!");
 };
 
+document.querySelector("#chat-message-submit").onclick = sendMessage;
 
-
-document.querySelector('#chat-message-submit').onclick = sendMessage;
-
-document.querySelector('#chat-message-input').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter' && !event.shiftKey) {
-        event.preventDefault();
-        sendMessage();
+document
+  .querySelector("#chat-message-input")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage();
     }
-});
+  });
 
 function sendMessage() {
-    const message = messageInputDom.value.trim();
+  const message = messageInputDom.value.trim();
 
-    if (message !== '') {
-        chatSocket.send(JSON.stringify({
-            'message': message,
-            'username': userName,
-            'room': roomName,
-            'avafiruser': dataMessages["avafiruser"],
-            'avasecuser': dataMessages["avasecuser"],
-            'firusername': dataMessages["firusername"],
-            'secusername': dataMessages["secusername"],
-        }));
-    }
+  if (message !== "") {
+    chatSocket.send(
+      JSON.stringify({
+        message: message,
+        username: userName,
+        room: roomName,
+        avafiruser: dataMessages["avafiruser"],
+        avasecuser: dataMessages["avasecuser"],
+        firusername: dataMessages["firusername"],
+        secusername: dataMessages["secusername"],
+      })
+    );
+  }
 
-    messageInputDom.value = '';
-    messageSubmitButton.disabled = true;
-    return false;
+  messageInputDom.value = "";
+  messageSubmitButton.disabled = true;
+  return false;
 }
 
 function scrollToBottom() {
-    const objDiv = document.querySelector('#chat-messages');
-    objDiv.scrollTop = objDiv.scrollHeight;
+  const objDiv = document.querySelector("#chat-messages");
+  objDiv.scrollTop = objDiv.scrollHeight;
 }
 
-window.onload = function() {
-    scrollToBottom();
-    const dataMessagesElem = document.getElementById("chat-messages");
-    dataMessages = dataMessagesElem.dataset;
+window.onload = function () {
+  scrollToBottom();
+  const dataMessagesElem = document.getElementById("chat-messages");
+  dataMessages = dataMessagesElem.dataset;
 };
