50e7592f5371a6120d96bdeaf1c1f65fa5266c37
Evgeniy <ekozlov00@mail.ru>
2024-01-31 20:08:16 +0600

    django set up

---
+++ new/Qt_Smart_device/django_digital_home/web/django_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django_app import views
+
+urlpatterns = [
+    path("", views.index),
+    path("settings/get/", views.settings_get),
+    path("settings/set/", views.settings_set),
+    path("settings/change/", views.settings_change, name="settings_change"),
+]
