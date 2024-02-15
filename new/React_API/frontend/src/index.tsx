84ffb9f7b61aa50363afee7923ecafefa6b4980f
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-02-15 16:30:58 +0600

    update

--- new/React_API/frontend/src/index.tsx
+++ new/React_API/frontend/src/index.tsx
@@ -6,18 +6,21 @@ import "./index.css";
 import axios from "axios";
 import Router from "./components/router";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
+function App1() {
+  // logic - логика (js)
+  //
+  // ...
+  return (
+    // design - дизайн часть (html+css)
+    <div></div>
+    // ...
+  );
+}
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Home />
-    </Provider>
-  </React.StrictMode>
-);
+function App() {
+  async function getData() {}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return NaN;
+}
+
+createRoot(document.getElementById("root")!).render(<Router />);
