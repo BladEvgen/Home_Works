edf8d369d5576e067cef0fad2ace0922ed5b474c
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-09 20:46:10 +0500

    ORM token

--- new/MineToken/django_app/models.py
+++ new/MineToken/django_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Token(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserExtend(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
