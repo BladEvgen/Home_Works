2a59be16a20c2120bbcc3e0958d8e0096e26740e
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-13 20:21:22 +0500

    models for block specific ips

--- new/MineToken/django_app/models.py
+++ new/MineToken/django_app/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Token(models.Model):
@@ -10,3 +12,30 @@ class Token(models.Model):
 
 class UserExtend(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.user.is_active = self.is_active
+        self.user.save()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return (
+            f"{self.user.username} - {'Разблокирован' if self.is_active else 'Забанен'}"
+        )
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    UserExtend.objects.get_or_create(user=instance)
+
+
+class FailedLoginAttempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ip_address = models.CharField(max_length=45)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class AccountLock(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    release_at = models.DateTimeField()
