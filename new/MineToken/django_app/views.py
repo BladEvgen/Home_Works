787080331d92cf5a13477b2b14b71bdd1162f089
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-13 20:21:58 +0500

    new token and added ip_address get for block user

--- new/MineToken/django_app/views.py
+++ new/MineToken/django_app/views.py
@@ -1,12 +1,22 @@
-import random
+import uuid
 
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from django_app.models import Token, UserExtend
-from django_app.utils import auth_required, password_check, username_check
+from django_app.utils import (
+    auth_required,
+    check_login_attempts,
+    delete_expired_failed_login_attempts,
+    is_account_locked,
+    lock_account,
+    log_failed_login_attempt,
+    password_check,
+    username_check,
+)
 
 """
 {
@@ -17,20 +27,43 @@ from django_app.utils import auth_required, password_check, username_check
 """
 
 
+@csrf_exempt
 @api_view(http_method_names=["POST"])
 def token(request):
     username = request.data.get("username", None)
     password = request.data.get("password", None)
+    ip_address = request.META.get("REMOTE_ADDR", None)
 
     if not password_check(password) or not username_check(username):
         return Response(data={"error": "Invalid"}, status=status.HTTP_401_UNAUTHORIZED)
 
     try:
         user = User.objects.get(username=username)
+        if is_account_locked(user):
+            Token.objects.filter(user=user).delete()
+            return Response(
+                data={"error": "Account locked, try again later."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
         if user.check_password(password):
-            token = "".join(random.choice(username) for _ in range(128))
+            token = "".join(str(uuid.uuid4()) for _ in range(4))
             Token.objects.create(user=user, token=token)
+
+            delete_expired_failed_login_attempts()
+
             return Response(data={"token": token})
+        else:
+            log_failed_login_attempt(user, ip_address)
+
+            if check_login_attempts(user, ip_address):
+                lock_account(user)
+                Token.objects.filter(user=user).delete()
+                return Response(
+                    data={"error": "Account locked, try again later."},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+
     except User.DoesNotExist:
         pass
 
