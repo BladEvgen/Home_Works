bfd18688e1a9f174fa373fe62ed715ed15601c06
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-04-11 21:00:49 +0500

    CRUD urls

--- new/Clothes/backend/clothes_app/urls.py
+++ new/Clothes/backend/clothes_app/urls.py
@@ -7,6 +7,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.home),
     path("api/messages/", views.warning_messages),
+    path("api/get_person_info", views.get_person_data_by_id),
+    path("api/delete_clothes", views.delete_clothes),
+    path("api/add_clothe", views.insert_new_clothe),
+    path("api/update_clothe", views.update_clothe),
 ]
 
 if settings.DEBUG:
